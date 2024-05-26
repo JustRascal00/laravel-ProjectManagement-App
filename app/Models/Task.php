3bd337e66a891f6c504482fe0eef2cfc4bb8985d
JustRascal00 <Rascal.vamp00@gmail.com>
2024-05-26 20:34:53 +0400

    update: Render Tasks

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     use HasFactory;
+    
+    public function project () {
+        return $this->belongsTo(Project::class);
+    }
+    public function assignedUser () {
+        return $this->belongsTo(Project::class, 'assigned_user_id');
+    }
+    public function createdBy () {
+        return $this->belongsTo(Project::class, 'created_by');
+    }
+    public function UpdatedBy () {
+        return $this->belongsTo(Project::class, 'updated_by');
+    }
 }
